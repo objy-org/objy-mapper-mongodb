9eb93ae94f236604e7ea440b8bcf09638f97fba4
Marco Boelling <marco.boelling@web.de>
2023-11-16 12:48:25 +0100

    set autocreate false

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var Admin = mongoose.mongo.Admin;
 
+mongoose.set('autoCreate', false);
+
 var clientSchema = {
     name: String
 };
